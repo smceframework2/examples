45832d62bbfab2c20a47de0edd930303bf5086a2
Samed Ceylan <imadige@gmail.com>
2015-03-03 12:01:22 +0200

    update

--- mvc/acl/main/controller/SiteController.php
+++ mvc/acl/main/controller/SiteController.php
@@ -12,15 +12,23 @@ class SiteController
 		$event->bind("acl",function(){
 
 			$acl= new Smce\Mvc\Acl;
-			$acl->setRules(array(
-			    'actions'=>array('index','page'), // Actions. is array
-			    'users'=>'@',  // Only * or @ values ​​are
-			    'redirect'=>"site/login",
-			    'expression'=>true,    //True is allowed only. Only TRUE or FALSE values ​​are.
-			    //'ip'=>array('127.0.0.1'), //IP is allowed only. is array
-			));
-			$acl->run();
-			return  $acl;
+			$acl->setRule(array(
+	                'actions'=>array('index','page'), // Actions. is array
+	                'users'=>'@',  // Only * or @ values ​​are
+	                'redirect'=>"site/login",
+	                'expression'=>true,    //True is allowed only. Only TRUE or FALSE values ​​are.
+	                //'ip'=>array('127.0.0.1'), //IP is allowed only. is array
+            ));
+
+            $acl->setRule(array(
+	                'actions'=>array('about'), // Actions. is array
+	                'users'=>'*',  // Only * or @ values ​​are
+	                'redirect'=>"site/login",
+	                'expression'=>true,    //True is allowed only. Only TRUE or FALSE values ​​are.
+	                //'ip'=>array('127.0.0.1'), //IP is allowed only. is array
+            ));
+            
+            $acl->run();
 		});
 
 	}
@@ -39,6 +47,14 @@ class SiteController
 	{
 
 		echo "Page";
+
+	}
+
+	// http://localhost/acl/site/about
+	public function actionAbout()
+	{
+
+		echo "About";
 
 	}
 
